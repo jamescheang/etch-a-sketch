f461288caa905f75e89aeb838abbe5573af2e8ea
jamescheang <cheang.james@gmail.com>
2023-03-10 18:01:09 +0800

    main grid area

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const drawingArea = document.getElementsByClassName("draw-area")[0];
 //function to set the drawing area size eg. 16x16 or 32x32
 function setPixels(num) {
     var pixel;
+    var totalColumns = "";
 
     pixels = num * num;
 
@@ -12,6 +13,17 @@ function setPixels(num) {
         pixel.className = "pixel";
         drawingArea.appendChild(pixel);
     }
+
+    for (let i = 0; i < num; i++) {
+        totalColumns += "auto "
+    }
+
+    drawingArea.style.gridTemplateColumns = totalColumns;
 }
 
-setPixels(3);
+setPixels(16); // change to use the value from the size slider
+
+// add event listener while hovering over element && left mouse button is clicked, change background colour of the pixel.
+function paintPixel() {
+
+}
